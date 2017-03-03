8071413d11df290121a5b4399a9129dd6fd04f57
Ali Chouria <ali.chouria@yahoo.fr>
2017-03-03 11:28:53 +0100

    modification AproposController

--- app/controllers/AproposController.java
+++ app/controllers/AproposController.java
@@ -6,4 +6,8 @@ import play.mvc.Controller;
  * Created by choural1 on 03/03/17.
  */
 public class AproposController extends Controller {
+
+    public static void apropos(){
+        render();
+    }
 }
