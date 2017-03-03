e26fda8b50e61c414e1367e68f94f3f06a47d49a
Ali Chouria <ali.chouria@yahoo.fr>
2017-03-03 14:38:16 +0100

    MAJ de controleur Apropos

--- app/controllers/AproposController.java
+++ app/controllers/AproposController.java
@@ -1,14 +1,6 @@
 package controllers;
 
-import com.google.gson.JsonObject;
-import play.data.validation.Email;
-import play.data.validation.Error;
-import play.data.validation.Required;
-import play.data.validation.Validation;
 import play.mvc.Controller;
-import play.mvc.results.RenderJson;
-
-import java.util.List;
 
 /**
  * Created by choural1 on 03/03/17.
