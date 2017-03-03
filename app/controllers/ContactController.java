e4308bc2a290b1a085f22f9be6b1bf0207188d3b
Ali Chouria <ali.chouria@yahoo.fr>
2017-03-03 16:13:27 +0100

    MAJ de controleur Contact

--- app/controllers/ContactController.java
+++ app/controllers/ContactController.java
@@ -1,15 +1,9 @@
 package controllers;
 
-import com.google.gson.JsonObject;
-import play.data.validation.Email;
-import play.data.validation.Error;
-import play.data.validation.Required;
 import play.data.validation.Validation;
 import play.mvc.Controller;
-import play.mvc.results.RenderJson;
 
 import javax.validation.Valid;
-import java.util.List;
 
 /**
  * Created by choural1 on 03/03/17.
@@ -20,25 +14,37 @@ public class ContactController  extends Controller{
         render();
     }
 
-    public static void validationFormulaireContact(@Valid UserContact user){
+//    public static void validationFormulaireContact(@Valid UserContact user){
+//        if (Validation.hasErrors()) {
+//            saveUserContact(Validation.errors());
+//        }
+////        redirect("index.html");
+//        ContactController.contact();
+//    }
+
+
+    public static void saveUserContact(@Valid UserContact userContact) {
         if (Validation.hasErrors()) {
-            apiValidationErrors(Validation.errors());
+            params.flash();
+            Validation.keep();
+            contact();
         }
-//        redirect("index.html");
-        ContactController.contact();
+        UserContactService.INSTANCE.save(userContact);
+        Application.index();
+
     }
 
-    protected static void apiValidationErrors(List<Error> errors) {
-        JsonObject json = new JsonObject();
-        json.addProperty("code", "ValidationErrors");
-        JsonObject jsonErrors = new JsonObject();
-        for (Error error : errors) {
-            jsonErrors.addProperty(error.getKey(), error.getMessageKey());
-        }
-        json.add("errors", jsonErrors);
-        response.status = 409;
-        throw new RenderJson(json);
-    }
+//    protected static void apiValidationErrors(List<Error> errors) {
+//        JsonObject json = new JsonObject();
+//        json.addProperty("code", "ValidationErrors");
+//        JsonObject jsonErrors = new JsonObject();
+//        for (Error error : errors) {
+//            jsonErrors.addProperty(error.getKey(), error.getMessageKey());
+//        }
+//        json.add("errors", jsonErrors);
+//        response.status = 409;
+//        throw new RenderJson(json);
+//    }
 
 }
 
