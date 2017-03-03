81e2911734b6c47dc8feda37204bb736b0719cd5
Ali Chouria <ali.chouria@yahoo.fr>
2017-03-03 16:14:48 +0100

    MAJ de controleur Contact

--- app/controllers/InscriptionController.java
+++ app/controllers/InscriptionController.java
@@ -1,6 +1,11 @@
 package controllers;
 
+import models.User;
+import play.data.validation.Valid;
+import play.data.validation.Validation;
 import play.mvc.Controller;
+
+import java.util.List;
 
 /**
  * Created by choural1 on 03/03/17.
@@ -10,4 +15,32 @@ public class InscriptionController extends Controller {
     public static void inscription(){
         render();
     }
+
+    public static void creerUser(@Valid User user) {
+        System.out.println(user.email);
+        if (Validation.hasErrors()) {
+            params.flash();
+            Validation.keep();
+            editUser(null);
+        }
+
+        user.save();
+        users();
+    }
+
+    public static void editUser(Long id) {
+        User user = null;
+        if(id != null) {
+            user = User.findById(id);
+        }
+        render(user);
+    }
+
+    private static void users() {
+            List<User> users = User.find("isSupprime = false").fetch();
+            List<User> usersSupprimes = User.find("isSupprime = true").fetch();
+            render(users, usersSupprimes);
+        }
+    }
+
 }
