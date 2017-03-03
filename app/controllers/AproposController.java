ffc8992ae4c003e2338cc4057ed733a7586ece5c
Ali Chouria <ali.chouria@yahoo.fr>
2017-03-03 14:36:41 +0100

    Ajout des fichiers de controleur Apropos et Contact

--- app/controllers/AproposController.java
+++ app/controllers/AproposController.java
@@ -1,6 +1,14 @@
 package controllers;
 
+import com.google.gson.JsonObject;
+import play.data.validation.Email;
+import play.data.validation.Error;
+import play.data.validation.Required;
+import play.data.validation.Validation;
 import play.mvc.Controller;
+import play.mvc.results.RenderJson;
+
+import java.util.List;
 
 /**
  * Created by choural1 on 03/03/17.
@@ -11,3 +19,4 @@ public class AproposController extends Controller {
         render();
     }
 }
+
--- app/controllers/ContactController.java
+++ app/controllers/ContactController.java
@@ -1,15 +1,42 @@
 package controllers;
 
+import com.google.gson.JsonObject;
+import play.data.validation.Email;
+import play.data.validation.Error;
+import play.data.validation.Required;
+import play.data.validation.Validation;
 import play.mvc.Controller;
+import play.mvc.results.RenderJson;
+
+import java.util.List;
 
 /**
  * Created by choural1 on 03/03/17.
  */
 public class ContactController  extends Controller{
 
-    public static void contact(){
+    public static void contact( @Required @Email String email,
+                                @Required String lastname,
+                                @Required String firstname,
+                                @Required String message){
+        if (Validation.hasErrors()) {
+            apiValidationErrors(Validation.errors());
+        }
         render();
     }
+
+    protected static void apiValidationErrors(List<Error> errors) {
+        JsonObject json = new JsonObject();
+        json.addProperty("code", "ValidationErrors");
+        JsonObject jsonErrors = new JsonObject();
+        for (Error error : errors) {
+            jsonErrors.addProperty(error.getKey(), error.getMessageKey());
+        }
+        json.add("errors", jsonErrors);
+        response.status = 409;
+        throw new RenderJson(json);
+    }
+
 }
 
 
