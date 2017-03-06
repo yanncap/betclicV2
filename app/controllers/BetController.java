35ce1a200cda162561074c17e8c70e56ccd10581
yann <yann.cap@live.com>
2017-03-06 11:35:03 +0100

    modification de BetController : récupération de la liste des paris

--- app/controllers/BetController.java
+++ app/controllers/BetController.java
@@ -1,12 +1,23 @@
 package controllers;
 
+import models.Bet;
 import play.mvc.Controller;
+import services.BetService;
+
+import java.util.List;
+
 
 /**
  * Created by choural1 on 03/03/17.
  */
 public class BetController extends Controller {
     public static void bet(){
-        render();
+        List<Bet> bets = BetService.INSTANCE.findAll();
+        if(bets == null || bets.isEmpty()){
+            //error
+        }
+
+        render(bets);
     }
+
 }
