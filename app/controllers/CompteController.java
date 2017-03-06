8ba1c12248eb9084f21a7fba37ddc6520f4794b5
Ali Chouria <ali.chouria@yahoo.fr>
2017-03-06 11:21:30 +0100

    Ajout compte

---
+++ app/controllers/CompteController.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import models.User;
+import play.data.validation.Valid;
+import play.data.validation.Validation;
+import play.mvc.Controller;
+
+/**
+ * Created by choural1 on 06/03/17.
+ */
+public class CompteController extends Controller{
+    public static void compte (){
+        render();
+    }
+
+
+    public static void creerCompteUser(@Valid User user) {
+        if (Validation.hasErrors()) {
+            params.flash();
+            Validation.keep();
+            editUser(null);
+        }
+
+        user.save();
+        Application.index();
+    }
+
+    public static void editUser(Long id) {
+        User user = null;
+        if(id != null) {
+            user = User.findById(id);
+        }
+        render(user);
+    }
+
+
+}
